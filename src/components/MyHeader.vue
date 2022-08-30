be2077e6418f1a27b27dd808ba9f9b02bbf4ba9a
pacimarco <m.paci1987@gmail.com>
2022-08-30 15:05:51 +0200

    prova logica, errori trasferimento dati

---
+++ src/components/MyHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <input type="text" v-model="query">
+            <button @click="search">Search</button>
+  </div>
+</template>
+
+<script>
+    
+export default {
+name:'MyHeader'
+
+}
+
+</script>
+
+<style>
+
+</style>
---
+++ src/components/MyMain.vue
@@ -0,0 +1,42 @@
+<template>
+  <main>
+    <ul>
+        <li class="result" v-for="result in results" :key="result.index">
+            <div class="title">{{ result.title }}</div>
+            <div class="original-title">{{ result.original_title }}</div>
+            <div class="language">{{ result.original_language }}</div>
+            <div class="vote">{{ result.vote_average }}</div>
+        </li>          
+    </ul>
+  </main>
+</template>
+
+<script>
+    import axios from 'axios';
+export default {
+name:'MyMain',
+
+
+data(){
+    return{
+        query: "",
+        
+    }
+},
+
+methods: {
+        search() {
+            axios
+            .get("https://api.themoviedb.org/3/search/movie?api_key=92311e9e4318df2bfa2b9bcfe8d9adfe&query=" + this.query)
+            .then(response => {
+                this.results = (response.data.results);
+                console.log(this.results);
+            })
+        }
+    }
+}    
+</script>
+
+<style>
+
+</style>
